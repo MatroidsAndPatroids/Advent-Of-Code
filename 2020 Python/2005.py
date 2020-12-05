e8a67715c1318f212a4913cc11859ec7ef44365a
Kristof K <kkristof424@gmail.com>
2020-12-05 07:55:13 +0100

    Refactor 2020 day 5

--- 2020 Python/2005.py
+++ 2020 Python/2005.py
@@ -1,57 +1,42 @@
 import utility # my own utility.pl file
 import re # match
 
-def seatNumber(boardingPass, part2 = False):
-    seatNumber = 0
-    magnitude = 64
-    
-    for c in boardingPass[:-3]:
-        if c == 'F':
-            seatNumber += 0
-        elif c == 'B':
-            seatNumber += magnitude
-        magnitude = int(magnitude / 2)
-    
-    seatNumber *= 8
-    magnitude = 4
-    for c in boardingPass[-3:]:
-        if c == 'L':
-            seatNumber += 0
-        elif c == 'R':
-            seatNumber += magnitude
-        magnitude = int(magnitude / 2)
-    
-    print(f'{boardingPass[:-3] = } {boardingPass[-3:] = } {seatNumber = }') 
-    return seatNumber
+# The seat number is the binary version of the boarding pass, eg.
+# 'BFFFBBFRRR' -> BIN 1000110111 -> DEC 567
+def seatNumber(boardingPass):
+    replaceTable = {"B": "1", "R": "1", "F": "0", "L": "0"}
+    replaceFunction = lambda character: replaceTable[character]
+    binary = ''.join(list(map(replaceFunction, boardingPass)))
+    return int(binary, 2)
 
-def highestSeat(boardingPasses, part2 = False):
-    highestSeat = 0
-    for bp in boardingPasses:
-        sN = seatNumber(bp, part2)
-        highestSeat = max(highestSeat, sN)
-    return highestSeat
+# Find the highest seat number in the list of boarding passes.
+def highestSeatNumber(boardingPasses):
+    existingSeatNumbers = list(map(seatNumber, boardingPasses))
+    return max(existingSeatNumbers)
 
+# My seat is the (only) empty seat on the plane.
+# Find the set of missing numbers amongst the list of seat numbers.
 def mySeatNumber(boardingPasses):
-    allSeats = list(range(1024))
-    for bp in boardingPasses:
-        sN = seatNumber(bp)
-        
-        if sN in allSeats:
-            #del allSeats.index(sN)
-            allSeats.remove(sN)
-    return allSeats
+    existingSeatNumbers = list(map(seatNumber, boardingPasses))
+    
+    highest = highestSeatNumber(boardingPasses)
+    lowest = min(existingSeatNumbers)
+    allPossibleSeatNumbers = list(range(lowest, highest - 1))
+    
+    missingSeats = set(allPossibleSeatNumbers) - set(existingSeatNumbers)
+    return missingSeats
 
 # Check test cases
 smallExample = [
     'BFFFBBFRRR',
     'FFFBBBFRRR',
     'BBFFBBFRLL']
-assert highestSeat(smallExample) == 820
+assert highestSeatNumber(smallExample) == 820
 
 # Display info message
-print("Give passport batch file:\n")
-passports = utility.readInputList()
+print("Give boarding passes:\n")
+boardingPasses = utility.readInputList()
 
 # Display results
-print(f'{highestSeat(passports) = }')
-print(f'{mySeatNumber(passports) = }')
+print(f'{highestSeatNumber(boardingPasses) = }')
+print(f'{mySeatNumber(boardingPasses) = }')
