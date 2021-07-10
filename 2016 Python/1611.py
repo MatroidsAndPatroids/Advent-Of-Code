3b509f990f49bc1ea2c35036e3723360eba80bb3
MatroidsAndPatroids <67026973+MatroidsAndPatroids@users.noreply.github.com>
2021-07-10 23:22:03 +0200

    Improved runtime from 40min to 0.12sec with A* algorithm.

--- 2016 Python/1611.py
+++ 2016 Python/1611.py
@@ -2,12 +2,18 @@ import utility # my own utility.pl file
 import re # compile, match
 import copy # deepcopy
 import time # process_time
+from networkx.algorithms.shortest_paths import astar_path
 from networkx.algorithms.shortest_paths.unweighted import bidirectional_shortest_path
 
 # Node object for the custom NetworkX graph object
 # It can calculate its own neighbour objects.
 class State:
-    states = 0
+    # Punish moving down items and reward moving them up
+    # This is pretty slow, so don't use it without heuristics :P
+    # calcWeight1 = {(-1, 2): 1000000, (-1, 1): 10000, (1, 1): 100, (1, 2): 1}
+    
+    # Moving only 1 up is infinitely worse than moving 1 down!?!? Yet it's optimal.
+    calcWeight = {(-1, 2): 1000000, (-1, 1): 30, (1, 1): 1000000, (1, 2): 1}
     
     def __init__(self, floor, chips, gens):
         self.floor = floor
@@ -81,7 +87,7 @@ class State:
     
     # Return the list of neighbouring states
     def getNeighbours(self):
-        neighbours = []
+        neighbours = {}
         nextFloors = self.getNextFloors()
         moveable = self.getMoveable()
         for toFloor in nextFloors:
@@ -89,37 +95,55 @@ class State:
                 newNeighbour = copy.deepcopy(self)
                 isValidMove = newNeighbour.move(m, toFloor)
                 if isValidMove:
-                    neighbours.append(newNeighbour)
-                    State.states += 1
+                    direction = toFloor - self.floor
+                    itemCount = len(m[0]) + len(m[1])
+                    weight = State.calcWeight[(direction, itemCount)]
+                    neighbours[newNeighbour] = {"weight" : weight}
         return neighbours
+    
+    # Difference of levels is the A* heuristic
+    def level(self):
+        itemCount = lambda floor : len(self.chips[floor]) + len(self.gens[floor])
+        # Originally I used floor * itemCount instead of 10**floor * itemCount,
+        # which was exponentially slower (35,000x slower for part 2)
+        #return sum(floor * itemCount(floor) for floor in self.floors())
+        return sum(10**floor * itemCount(floor) for floor in self.floors())
 
 # Custom NetworkX graph object
-# Implements only the necessary functions for bidirectional_shortest_path
+# Implements functions for astar_path
 class RadioisotopeTestingFacility:
-    class AdjacencyWrapper:
-        def __getitem__(self, state):
-            return state.getNeighbours()
-        
-    def __init__(self):
-        self.adj = RadioisotopeTestingFacility.AdjacencyWrapper()
+    states = 0
     
+    def __init__(self):
+        self.adj = self
+    
+    def __getitem__(self, state):
+        RadioisotopeTestingFacility.states += 1
+        return state.getNeighbours()
+        
     def __contains__(self, state):
         return state.isValid()
     
     def is_directed(self):
         return False
+    
+    def is_multigraph(self):
+        return False
 
 # Custom Timer object, prints out infos upon destruction
 class PathTimer:
     def __init__(self):
-        State.states = 0
+        RadioisotopeTestingFacility.states = 0
         self.begin = time.process_time()
         
     def __del__(self):
+        states = RadioisotopeTestingFacility.states
         elapsed = time.process_time() - self.begin
-        print(f'States visited: {State.states}, Process time: {elapsed} seconds')
+        print(f'States visited: {states}, Process time: {elapsed} seconds')
 
 
+# Parse the inputfile to get the source state
+# Abbreviate each item with its 2nd letter, eg. H = Thulium, L = Plutonium, etc.
 def getSourceState(instructions):
     floor = 0
     chips = []
@@ -164,7 +188,25 @@ def getTargetState(state):
 
     return targetState
 
-def shortestPath(instructions, part2 = False):
+# Difference of the level potential function heuristics for A*, this one rocks
+def levelRise(s1, s2):
+    return abs(s1.level() - s2.level())
+
+# Sum the number of floor swaps for all items and the elevator
+# This heuristic has proven entirely useless :-)
+def swapCount(s1, s2):
+    swaps = abs(s1.floor - s2.floor)
+    for d1, d2 in ((s1.chips, s2.chips), (s1.gens, s2.gens)):
+        dict = {}
+        for floor in s1.floors():
+            for item in d1[floor]:
+                dict[item] = floor
+        for floor in s2.floors():
+            for item in d2[floor]:
+                swaps += abs(dict[item] - floor)
+    return swaps
+
+def shortestPath(instructions, part2=False, bidirectional=False):
     timer = PathTimer()
     
     RTF = RadioisotopeTestingFacility()
@@ -177,7 +219,11 @@ def shortestPath(instructions, part2 = False):
         source.gens[0].update({'E', 'D'})
     target = getTargetState(source)
     
-    path = bidirectional_shortest_path(RTF, source, target)
+    if bidirectional:
+        # Ye olde bidirectional search (slow)
+        path = bidirectional_shortest_path(RTF, source, target)
+    else:
+        path = astar_path(RTF, source, target, heuristic=levelRise, weight="weight")
     for index, state in enumerate(path):
         print(index, state)
 
@@ -199,7 +245,42 @@ instructions = utility.readInputList()
 path = shortestPath(instructions)
 path = shortestPath(instructions, part2 = True)
 
-# Timer results:
-# States visited:      311, Process time:    0.03125 seconds
-# States visited:   123455, Process time:   17.328125 seconds
-# States visited: 12740306, Process time: 2377.109375 seconds
+# Timer results (bidirectional_shortest_path):
+# States visited:     111, Process time:    0.03125 seconds
+# States visited:   31592, Process time:   18.421875 seconds
+# States visited: 2358241, Process time: 2423.328125 seconds
+
+# Timer results (astar_path with levelRise1 heuristic and weight1):
+# States visited:     85, Process time:   0.03125 seconds
+# States visited:  24544, Process time:  16.234375 seconds
+# States visited: 861281, Process time: 887.625 seconds
+
+# Timer results (astar_path with weight1 only):
+# States visited:    101, Process time:   0.03125 seconds
+# States visited:  24744, Process time:  16.515625 seconds
+# States visited: 861945, Process time: 887.34375 seconds
+
+# Timer results (astar_path with levelRise1 heuristic only):
+# States visited:      72, Process time:    0.015625 seconds
+# States visited:  112336, Process time:   80.0625 seconds
+# States visited: 5602114, Process time: 6874.609375 seconds (YIKES!)
+
+# Timer results (astar_path with levelRise2 heuristic only):
+# States visited:  16, Process time: 0.015625 seconds
+# States visited: 102, Process time: 0.0625 seconds
+# States visited: 283, Process time: 0.203125 seconds (WOW!!!)
+
+# Timer results (astar_path with levelRise2 heuristic and weight2):
+# States visited:  14, Process time: 0.015625 seconds
+# States visited:  62, Process time: 0.03125 seconds
+# States visited: 203, Process time: 0.125 seconds (THE BEST so far)
+
+# Timer results (astar_path with weight2 only):
+# States visited:     58, Process time:   0.015625 seconds
+# States visited:   9691, Process time:   6.0 seconds
+# States visited: 558456, Process time: 625.65625 seconds
+
+# Timer results (astar_path with no heuristic, no weight):
+# States visited:    345, Process time:   0.09375 seconds
+# States visited: 158158, Process time: 105.140625 seconds
+# Ran out of memory (>2hours, around 8GB) :)
